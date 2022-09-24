81b0d4a43c64f24363bb68fb147aa84955ff45ad
Egor <vegormak7@gmail.com>
2022-09-25 00:28:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+console.log(4+ +'7');
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(7%2);
+
+console.log(4*3 == '12');
+console.log(4*3 === 12);
+
+console.log(2 + 2 *2 != 8);  // != - не равен
+
+// && и
+// || или
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+    
+    
